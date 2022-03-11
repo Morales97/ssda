923a5eaead8c62b1f48aa444695c13450ea7f97b
Morales97 <danimoralesbrotons@gmail.com>
2022-03-11 17:57:58 +0100

    Create simba_run.sh

---
+++ simba_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=seg_test
+#
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30000
+#SBATCH --time=06:00:00
+
+
+python main.py --expt_name=cs_pretrain
+
