36297e1302a1a69a31a3a13d82aef38318dba8a7
Morales97 <danimoralesbrotons@gmail.com>
2022-03-11 11:28:18 +0100

    Update cityscapes_loader.py

--- loader/cityscapes_loader.py
+++ loader/cityscapes_loader.py
@@ -157,7 +157,8 @@ class cityscapesLoader(data.Dataset):
 
         img = pil_loader(img_path, self.img_size[1], self.img_size[0])
         img = np.array(img, dtype=np.uint8)
-        img.transpose(2, 0, 1) # (C, W, H)
+        pdb.set_trace()
+        #img.transpose(2, 0, 1) # (C, W, H)
 
         lbl = pil_loader(lbl_path, self.img_size[1], self.img_size[0], is_segmentation=True)
         lbl = self.encode_segmap(np.array(lbl, dtype=np.uint8))
