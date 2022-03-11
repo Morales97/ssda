0104f0b52e425d4ce05d093c60c9c27d6c75d5ef
Morales97 <danimoralesbrotons@gmail.com>
2022-03-11 13:17:47 +0100

    .

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -33,6 +33,7 @@ class runningScore(object):
 
     def update(self, label_trues, label_preds):
         for lt, lp in zip(label_trues, label_preds):
+            pdb.set_trace()
             self.confusion_matrix += self._fast_hist(lt.flatten(), lp.flatten(), self.n_classes)
 
     def get_scores(self):
