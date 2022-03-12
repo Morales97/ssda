3112ec2791f51b93969b0ffbd7f01e86fb480ff9
Morales97 <danimoralesbrotons@gmail.com>
2022-03-12 10:44:58 +0100

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -11,5 +11,5 @@
 #SBATCH --time=06:00:00
 
 
-python main.py --expt_name=deeplabv3_mnv3 --net=dl_mobilenet --pre_trained=False  --pre_trained_backbone=True
+python main.py --expt_name=deeplabv3_mnv3_lr2 --net=dl_mobilenet --pre_trained=False  --pre_trained_backbone=True --lr=0.01
 
