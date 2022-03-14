4805b4e77b1bc561f2b109498f31ff636252b3ca
Morales97 <danimoralesbrotons@gmail.com>
2022-03-14 17:51:04 +0100

    Update test_oct.py

--- test_oct.py
+++ test_oct.py
@@ -18,7 +18,7 @@ from utils.ioutils import gen_unique_name
 from utils.ioutils import get_log_str
 from utils.ioutils import parse_args
 from utils.ioutils import rm_format
-from loader.oct_loader import octLoader
+from loader.oct_loader import octLoader, gtaLoader
 from loss.loss import cross_entropy2d
 from evaluation.metrics import averageMeter, runningScore
 import wandb
@@ -34,10 +34,10 @@ def main(args, wandb):
     #np.random.seed(args.seed)
     random.seed(args.seed)
 
-    t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Spectralis_part1', label_path='data/retouch-dataset/pre_processed/Spectralis_part1', img_size=(512, 512))
+    #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Spectralis_part1', label_path='data/retouch-dataset/pre_processed/Spectralis_part1', img_size=(512, 512))
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Cirrus_part1', label_path='data/retouch-dataset/pre_processed/Cirrus_part1', img_size=(512, 512))
     #v_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='val')
-    #s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/cityscapes/labels', img_size=(360, 680))
+    s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/cityscapes/labels', img_size=(360, 680))
 
 
 if __name__ == '__main__':
