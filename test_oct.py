d3562410360b25507f4421243e304311832c1f7c
Morales97 <danimoralesbrotons@gmail.com>
2022-03-14 17:52:37 +0100

    Update test_oct.py

--- test_oct.py
+++ test_oct.py
@@ -38,7 +38,7 @@ def main(args, wandb):
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Cirrus_part1', label_path='data/retouch-dataset/pre_processed/Cirrus_part1', img_size=(512, 512))
     #v_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='val')
     s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/cityscapes/labels', img_size=(360, 680))
-
+    s_loader.test()
 
 if __name__ == '__main__':
     args = parse_args()
