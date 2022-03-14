5a31df2bf3487af33c0cda421df2284ee61d67e2
Morales97 <danimoralesbrotons@gmail.com>
2022-03-14 18:55:16 +0100

    Update test_oct.py

--- test_oct.py
+++ test_oct.py
@@ -38,10 +38,10 @@ def main(args, wandb):
 
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Spectralis_part1', label_path='data/retouch-dataset/pre_processed/Spectralis_part1', img_size=(512, 512))
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Cirrus_part1', label_path='data/retouch-dataset/pre_processed/Cirrus_part1', img_size=(512, 512))
-    v_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='val')
-    v_loader.test()
-    #s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/gta5/labels', img_size=(360, 680))
-    #s_loader.test()
+    #v_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='val')
+    #v_loader.test()
+    s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/gta5/labels', img_size=(360, 680))
+    s_loader.test()
 
     loader = DataLoader(
         s_loader,
