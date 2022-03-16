11a51650b95493f0c80ed030862de89cbf8ce90b
Morales97 <danimoralesbrotons@gmail.com>
2022-03-16 10:25:22 +0100

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -12,5 +12,5 @@
 
 
 # python main.py --expt_name=lraspp_mnv3 --net=lraspp_mobilenet --pre_trained=False  --pre_trained_backbone=True --steps=5000
-python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_S_and_T_200 --net=lraspp_mobilenet --target_samples=200 --batch_size=8
+python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_S_and_T_200_NEW_LOADER --net=lraspp_mobilenet --target_samples=200 --batch_size=8
 
