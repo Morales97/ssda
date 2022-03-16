47a6e88b3597b66d8aa6b0d97c18c18e6ef6cfc1
Morales97 <danimoralesbrotons@gmail.com>
2022-03-16 10:36:13 +0100

    .

--- test_oct.py
+++ test_oct.py
@@ -38,17 +38,17 @@ def main(args, wandb):
 
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Spectralis_part1', label_path='data/retouch-dataset/pre_processed/Spectralis_part1', img_size=(512, 512))
     #t_loader = octLoader(image_path='data/retouch-dataset/pre_processed/Cirrus_part1', label_path='data/retouch-dataset/pre_processed/Cirrus_part1', img_size=(512, 512))
-    #t_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='train', few_samples=100)
+    t_loader = cityscapesLoader(image_path='data/cityscapes/leftImg8bit_tiny', label_path='data/cityscapes/gtFine', img_size=(256, 512), split='train', few_samples=100)
     #v_loader.test()
-    s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/gta5/labels', img_size=(360, 640), split='val', rotation=False)
+    #s_loader = gtaLoader(image_path='data/gta5/images_tiny', label_path='data/gta5/labels', img_size=(360, 640), split='val', rotation=False)
     #s_loader = gtaLoader(image_path='data/images', label_path='data/labels', img_size=(360, 640))
     #s_loader.test()
 
     loader = DataLoader(
-        s_loader,   # TODO there will be the problem of different img sizes in the same batch
+        t_loader,   
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        shuffle=True,  # TODO change to True
+        shuffle=False, 
     )
 
     for (images, labels) in loader:
