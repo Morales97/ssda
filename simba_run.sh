b7fe99b28200624244d8dfd7aefc54d862c62963
Morales97 <danimoralesbrotons@gmail.com>
2022-03-17 10:52:55 +0100

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -14,10 +14,10 @@
 # python main.py --expt_name=lraspp_mnv3 --net=lraspp_mobilenet --pre_trained=False  --pre_trained_backbone=True --steps=5000
 
 # IN backbone
-python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size --net=lraspp_mobilenet --target_samples=100 --batch_size=8
+#python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size --net=lraspp_mobilenet --target_samples=100 --batch_size=8
 
 # Rotation pretrained backbone
-#python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_rot_pt --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=ckpt_rot_10k.tar
+python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size_rot_pt --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=ckpt_rot_10k.tar
 
 # Rotation
 # python pretrain/rotation.py --batch_size=4 --lr=0.001 --project=rotation --expt_name=longer_run_lr_x10 --save_dir=pretrain/expts_rot/tmp_last --save_interval=2000 --steps=10001
