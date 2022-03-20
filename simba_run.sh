20e2b046fcfbd5d7bf1786007633cb5eef672acd
Morales97 <danimoralesbrotons@gmail.com>
2022-03-20 09:31:14 +0100

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -8,14 +8,14 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
 #SBATCH --mem=30000
-#SBATCH --time=12:00:00
+#SBATCH --time=14:00:00
 
 # ----- Fully supervised CS -----
 #python main.py --project=GTA_to_CS_tiny --expt_name=deeplab_rn50_FS_CS_pt_mask --net=deeplabv3_mask_pt --steps=5000
 #python main.py --project=GTA_to_CS_tiny --expt_name=fcn_rn50_FS_CS --net=resnet50_fcn --steps=5000
 
 # ----- Domain Adaptation -----
-python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_deeplab --net=deeplabv3 --target_samples=100 --batch_size=8
+python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_deeplab_lr3 --net=deeplabv3 --target_samples=100 --batch_size=8 --lr=0.001
 
 # Rotation pretrained backbone
 #python main_S_and_T_2.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size_rot_pt --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=ckpt_rot_10k.tar
