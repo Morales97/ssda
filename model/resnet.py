60c5726a0bc1725ce4e94a3dfff12f0fcfccdcd8
Morales97 <danimoralesbrotons@gmail.com>
2022-03-24 21:24:53 +0100

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -56,8 +56,8 @@ def lraspp_mobilenetv3_large(pretrained=False, pretrained_backbone=True, custom_
                 if k.startswith('backbone.0'):
                     new_key = k.rsplit('.0')[0] + k.rsplit('.0')[1]     # remove '.0'
                     new_dict[new_key] = v 
-        '''
-        elif 'model' in checkpoint.keys():
+        
+        elif False: #'model' in checkpoint.keys():
             # MaskContrast pretrain with head embedding dim = 32
             state_dict = checkpoint['model']
 
@@ -68,7 +68,6 @@ def lraspp_mobilenetv3_large(pretrained=False, pretrained_backbone=True, custom_
                 if k.startswith('model_q.decoder') and 'low' not in k and 'high' not in k:
                     new_dict['classifier' + k.rsplit('model_q.decoder')[1]] = v
 
-        '''
         elif 'model' in checkpoint.keys():
             # MaskContrast pretrain with head embedding dim = n_classes
             state_dict = checkpoint['model']
