1d176d037b72b1d94585dd08bcf5ca19d8186361
Morales97 <danimoralesbrotons@gmail.com>
2022-04-01 12:31:50 +0200

    Update loss.py

--- loss/loss.py
+++ loss/loss.py
@@ -4,11 +4,19 @@ import torch.nn.functional as F
 
 def cross_entropy2d(input, target, weight=None, size_average=True):
     n, c, h, w = input.size()
-    nt, ht, wt = target.size()
 
-    # Handle inconsistent size between input and target
-    if h != ht and w != wt:  # upsample labels
-        input = F.interpolate(input, size=(ht, wt), mode="bilinear", align_corners=True)
+    if len(target.size()) == 3:
+        nt, ht, wt = target.size()
+
+        # Handle inconsistent size between input and target
+        if h != ht and w != wt:  # upsample labels
+            input = F.interpolate(input, size=(ht, wt), mode="bilinear", align_corners=True)
+    
+    elif len(target.size()) == 1: # already flattened
+        assert target.size() == n*h*w
+
+    else:
+        raise Exception('check target size')
 
     input = input.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
     target = target.view(-1)
@@ -16,3 +24,4 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
         input, target, weight=weight, size_average=size_average, ignore_index=250
     )
     return loss
+
