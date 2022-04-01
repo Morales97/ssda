1dbecb4e2c54a5dede11ae2d17691dc5bc3d803e
Morales97 <danimoralesbrotons@gmail.com>
2022-04-01 17:51:00 +0200

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -18,7 +18,8 @@
 # -- DeepLab --
 # python main_dummyDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_PASCAL_lr3 --net=deeplabv3_mask_pt --target_samples=100 --batch_size=8 --custom_pretrain_path=model/pretrained/VOCSegmentation_supervised_saliency_model.pth.tar 
 # -- LR-ASPP --
-#python main_dummyDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_600 --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_600 --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=no_pt_GTA_crops --net=lraspp_mobilenet --target_samples=100 --batch_size=8 
 
 # Rotation pretrained backbone
 #python main_dummyDA.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size_rot_pt --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --custom_pretrain_path=ckpt_rot_10k.tar
@@ -28,4 +29,4 @@
 
 
 # ----- SSDA -----
-python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_no_pt_prob_distr --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --cr=prob_distr
+#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_no_pt_prob_distr --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --cr=prob_distr
