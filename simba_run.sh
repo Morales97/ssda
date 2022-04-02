5d06b2fb4b6b6608360989b9241ec540867fb91d
Morales97 <danimoralesbrotons@gmail.com>
2022-04-02 12:47:11 +0200

    Update simba_run.sh

--- simba_run.sh
+++ simba_run.sh
@@ -30,4 +30,4 @@
 
 # ----- SSDA -----
 #python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_no_pt_prob_distr --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --cr=prob_distr
-python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_one_hot_mask_pt_300_CS_GTA --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_GTA.pth.tar
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_prob_distr_mask_pt_300_CS_GTA --net=lraspp_mobilenet --target_samples=100 --batch_size=8 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_GTA.pth.tar
