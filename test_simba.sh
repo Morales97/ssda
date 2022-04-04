6670f52a391c9a48d2cffc45b82e66396d83eeb2
Morales97 <danimoralesbrotons@gmail.com>
2022-04-04 18:20:37 +0200

    Create test_simba.sh

---
+++ test_simba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=test_simba
+#
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem=30000
+#SBATCH --time=01:00:00
+
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=expt_1 --net=lraspp_mobilenet --target_samples=100 --cr=one_hot --lmbda=5
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=expt_2 --net=lraspp_mobilenet --target_samples=100 --cr=one_hot --lmbda=5
