a6ba6b13ee8c1382599c6fb0ab4f3d939206a671
Morales97 <danimoralesbrotons@gmail.com>
2022-04-04 15:49:50 +0200

    .

--- simba_run.sh
+++ simba_run.sh
@@ -29,7 +29,6 @@
 
 
 # ----- SSDA -----
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_no_pt_prob_distr_lambda5 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --lmbda=5
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_no_pt_one_hot_lambda5 --net=lraspp_mobilenet --target_samples=100 --cr=one_hot --lmbda=5
 #python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_prob_distr_mask_pt_300_CS_GTA --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_GTA.pth.tar
-python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_prob_distr_mask_pt_CS_NO_GTA --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
 
