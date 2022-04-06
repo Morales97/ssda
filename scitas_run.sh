4e2dc5c4967dc024a6c81501472496a7f060b737
Morales97 <danimoralesbrotons@gmail.com>
2022-04-06 17:38:43 +0200

    Update scitas_run.sh

--- scitas_run.sh
+++ scitas_run.sh
@@ -33,4 +33,4 @@
 
 # ----- SSDA -----
 #python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_prob_distr_mask_pt_CS_NO_GTA --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
-python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_IN_bb_CS_GTA --seed=2 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr #--custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_GTA_300.tar
+python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_IN_bb_CS_GTA --seed=3 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr #--custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_GTA_300.tar
