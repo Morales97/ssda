fe87c92dbe474760ef0f5b5334e164e0aede8d6e
Morales97 <danimoralesbrotons@gmail.com>
2022-04-06 09:57:07 +0200

    Update two_jobs_simba.sh

--- two_jobs_simba.sh
+++ two_jobs_simba.sh
@@ -10,7 +10,7 @@
 #SBATCH --mem=30000
 #SBATCH --time=03:00:00
 
-CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=IN_bb_s4 --seed=4 --net=lraspp_mobilenet --target_samples=100 & #--custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
-CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=IN_bb_s5 --seed=5 --net=lraspp_mobilenet --target_samples=100 #--custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_s4 --seed=4 --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
+CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_s5 --seed=5 --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
 
 #wait
