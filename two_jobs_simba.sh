726f6421fb11f0c446f4840f76d3244c6608ad1f
Morales97 <danimoralesbrotons@gmail.com>
2022-04-06 11:36:15 +0200

    Update two_jobs_simba.sh

--- two_jobs_simba.sh
+++ two_jobs_simba.sh
@@ -10,8 +10,12 @@
 #SBATCH --mem=30000
 #SBATCH --time=03:00:00
 
-CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_pd_IN_bb_s3 --seed=3 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr &#--custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
-CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_mask_pt_CS_s1 --seed=1 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_mask_pt_CS_s2 --seed=2 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
+CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_mask_pt_CS_s3 --seed=3 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+
+#CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_pd_IN_bb_s3 --seed=3 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr &#--custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
+#CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_mask_pt_CS_s1 --seed=1 --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+
 
 #CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_s4 --seed=4 --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
 #CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_s5 --seed=5 --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
