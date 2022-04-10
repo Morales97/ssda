2ad67e50163f47b754eaeb537ec54185e3b43643
Morales97 <danimoralesbrotons@gmail.com>
2022-04-10 20:22:08 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -9,6 +9,7 @@ from torchsummary import summary
 from model.fcn import fcn8s, fcn_resnet50, fcn_resnet50_densecl
 from model.deeplab import deeplabv3_resnet50_maskContrast, deeplabv3_rn50, deeplabv3_mobilenetv3_large
 from model.lraspp import lraspp_mobilenetv3_large
+from model.lraspp_contrast import lraspp_mobilenet_v3_large_contrast
 
 def get_model(args):
     if args.net == 'fcn_resnet50':
@@ -25,6 +26,8 @@ def get_model(args):
         model = deeplabv3_mobilenetv3_large(args.pre_trained, args.pre_trained_backbone)
     elif args.net == 'lraspp_mobilenet':
         model = lraspp_mobilenetv3_large(args.pre_trained, args.pre_trained_backbone, args.custom_pretrain_path)
+    elif args.net == 'lraspp_mobilenet_v3_large_contrast':
+        model = lraspp_mobilenet_v3_large_contrast(args.pre_trained, args.pre_trained_backbone)
 
     return model
 
