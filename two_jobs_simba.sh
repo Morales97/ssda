61625e99a4c74a0998751d1c2d4d0f4d2eb612c4
Morales97 <danimoralesbrotons@gmail.com>
2022-04-10 13:53:00 +0200

    Update two_jobs_simba.sh

--- two_jobs_simba.sh
+++ two_jobs_simba.sh
@@ -11,8 +11,8 @@
 #SBATCH --time=03:00:00
 
 CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=mask_pt_CS_sup_s2 --seed=2 --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s2.tar &
-CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=mask_pt_CS_sup_s3 --seed=3 --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s3.tar &
-#CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_mask_pt_CS_sup_s2 --seed=2 --cr=prob_distr --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s2.tar
+CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=mask_pt_CS_sup_s3 --seed=3 --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s3.tar 
+#CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_mask_pt_CS_sup_s2 --seed=2 --cr=prob_distr --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s2.tar &
 #CUDA_VISIBLE_DEVICES=1 python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_mask_pt_CS_sup_s3 --seed=3 --cr=prob_distr --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_sup_s3.tar
 
 #CUDA_VISIBLE_DEVICES=0 python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_pd_IN_bb_s3 --seed=3 --cr=prob_distr &#--custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar &
