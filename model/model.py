bf080602203a47c93a953a8604055c1b7acbdec7
Morales97 <danimoralesbrotons@gmail.com>
2022-04-10 18:11:21 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -10,7 +10,7 @@ from model.fcn import fcn8s, fcn_resnet50, fcn_resnet50_densecl
 from model.deeplab import deeplabv3_resnet50_maskContrast, deeplabv3_rn50, deeplabv3_mobilenetv3_large
 from model.lraspp import lraspp_mobilenetv3_large
 
-def get_model(args)
+def get_model(args):
     if args.net == 'fcn_resnet50':
         model = fcn_resnet50(args.pre_trained)
     elif args.net == 'denseCL_fcn_rn50':
