81cdeb02f5d6ccd4ef9e60c1ff95ba34f6fcaa6a
Morales97 <danimoralesbrotons@gmail.com>
2022-04-12 17:46:13 +0200

    Update scitas_run_dl.sh

--- scitas_run_dl.sh
+++ scitas_run_dl.sh
@@ -12,7 +12,7 @@
 #SBATCH --time=12:00:00
 
 
-python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_mask_CS --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS.tar --cr=prob_distr #--pixel_contrast=True
+python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_CS --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS.tar # --cr=prob_distr #--pixel_contrast=True
 
 #python main_FS.py --project=GTA_to_CS_tiny --expt_name=FS_IN_bb --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 
 #python main_FS.py --project=GTA_to_CS_small --size=small --expt_name=FS_IN_bb --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 
