122b089e6765cd3946971e3bf996e369c1e77d2c
Morales97 <danimoralesbrotons@gmail.com>
2022-04-13 22:57:11 +0200

    Update scitas_run_dl.sh

--- scitas_run_dl.sh
+++ scitas_run_dl.sh
@@ -12,7 +12,8 @@
 #SBATCH --time=12:00:00
 
 #python main_SSDA.py --save_model=False --project=GTA_to_CS_tiny --expt_name=CR_IN_bb_bs2 --net=deeplabv3_rn50 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --seed=1 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
-python main_SSDA.py --save_model=False --project=GTA_to_CS_tiny --expt_name=IN_bb_lr_poly --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
+#python main_SSDA.py --save_model=False --project=GTA_to_CS_tiny --expt_name=IN_bb_lr_poly --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
+python main_FS.py --save_model=True --save_interval=5000 --steps=25000 --project=GTA_to_CS_tiny --expt_name=FS_25k_lr2 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
 
 #python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_JS_IN_bb_2_augs_detach --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --cr=js --tau=0 --n_augmentations=2 
 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS.tar 
