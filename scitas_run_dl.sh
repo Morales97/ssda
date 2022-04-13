c231725371295998e24905487d3258918fed1709
Morales97 <danimoralesbrotons@gmail.com>
2022-04-13 15:39:47 +0200

    Update scitas_run_dl.sh

--- scitas_run_dl.sh
+++ scitas_run_dl.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem=30000
 #SBATCH --time=12:00:00
 
-python main_SSDA.py --save_model=False --project=GTA_to_CS_tiny --expt_name=mask_CS_400 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
+python main_SSDA.py --save_model=False --project=GTA_to_CS_tiny --expt_name=CR_JS_mask_CS_400 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --cr=js --custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
 
 #python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_JS_IN_bb_2_augs_detach --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --seed=1 --cr=js --tau=0 --n_augmentations=2 
 #--custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS.tar 
