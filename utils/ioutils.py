e38a450d143ceb4ca6b4c916e4a265063a869162
Morales97 <danimoralesbrotons@gmail.com>
2022-04-14 11:10:29 +0200

    Update ioutils.py

--- utils/ioutils.py
+++ utils/ioutils.py
@@ -71,7 +71,7 @@ def get_parser():
                         help='threshold for pseudolabels')
     parser.add_argument('--lmbda', type=float, default=1,
                         help='weight of consistency regularization loss')
-    parser.add_argument('--aug_level', type=int, default=3,
+    parser.add_argument('--aug_level', type=int, default=4,
                         help='Strong augmentations level')  
     parser.add_argument('--n_augmentations', type=int, default=1,
                         help='number of augmentations in CR')                       
