0421fddb6d3c4cacb2be6bbe551ec37a7065b9ac
Morales97 <danimoralesbrotons@gmail.com>
2022-04-26 09:56:59 +0200

    Update main_SSDA.py

--- main_SSDA.py
+++ main_SSDA.py
@@ -107,11 +107,12 @@ def main(args, wandb):
         labels_s = labels_s.cuda()
         images_t = images_t.cuda()
         labels_t = labels_t.cuda()
+        save_image(images_s, 'gta_lab_orig.jpg')
 
         images_s = lab_transform(images_s, images_t)
         from torchvision.utils import save_image
-        save_image(images_s, 'gta_lab' + str(step) + '.jpg')
-        save_image(images_t, 'cs_lab' + str(step) + '.jpg')
+        save_image(images_s, 'gta_lab.jpg')
+        save_image(images_t, 'cs_lab.jpg')
         
         pdb.set_trace()
 
