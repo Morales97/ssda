1975eda8d0661e9418fa242bb2092d09208db44d
Morales97 <danimoralesbrotons@gmail.com>
2022-05-04 08:52:06 +0200

    Update scitas_run_dl.sh

--- scitas_run_dl.sh
+++ scitas_run_dl.sh
@@ -12,7 +12,7 @@
 #SBATCH --time=04:00:00
 
 #python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=JS_EMA_mask --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=js --custom_pretrain_path=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
-python main_SSDA.py --seed=2 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=KL_oh --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl_oh --lmbda=0.05
+python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=KL_oh --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl_oh --lmbda=0.05
 
 #python main_SSDA.py --seed=1 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=dsbn_l1_alonso_tu_0.9 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --alonso_contrast=base --dsbn=True
 #python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=alonso_FQ --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --alonso_contrast=feat_quality
