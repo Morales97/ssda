1883695c156e14bcea7ccae5399b707ebf627b59
Morales97 <danimoralesbrotons@gmail.com>
2022-05-05 10:09:55 +0200

    .

--- loss/pixel_contrast_unsup.py
+++ loss/pixel_contrast_unsup.py
@@ -107,49 +107,69 @@ def contrastive_class_to_class(model, features, class_labels, memory, use_select
         features_c = features[mask_c,:]
         memory_c = memory[c] # N, 256
 
-        # get the self-attention MLPs both for memory features vectors (projected vectors) and network feature vectors (predicted vectors)
-        selector = model.__getattr__('contrastive_class_selector_' + str(c))
-        #selector_memory = model.__getattr__('contrastive_class_selector_memory' + str(c))
+        if use_selector:
+            # get the self-attention MLPs both for memory features vectors (projected vectors) and network feature vectors (predicted vectors)
+            selector = model.__getattr__('contrastive_class_selector_' + str(c))
+            selector_memory = model.__getattr__('contrastive_class_selector_memory' + str(c))
 
-        if memory_c is not None and features_c.shape[0] > 1 and memory_c.shape[0] > 1:
+            if memory_c is not None and features_c.shape[0] > 1 and memory_c.shape[0] > 1:
+                memory_c = torch.from_numpy(memory_c).cuda()
 
-            memory_c = torch.from_numpy(memory_c).cuda()
+                # L2 normalize vectors
+                memory_c = F.normalize(memory_c, dim=1) # N, 256
 
-            # L2 normalize vectors
-            memory_c = F.normalize(memory_c, dim=1) # N, 256
+                use_prototypes = False
+                if use_prototypes:
+                    prototype = features_c.mean(dim=0).unsqueeze(0)  # M=1, 256
+                    features_c_norm = F.normalize(prototype, dim=1) # M=1, 256
+                else:
+                    features_c_norm = F.normalize(features_c, dim=1) # M, 256
+                
+                # compute similarity. All elements with all elements
+                similarities = torch.mm(features_c_norm, memory_c.transpose(1, 0))  # MxN
+                distances = 1 - similarities # values between [0, 2] where 0 means same vectors
+                # M (elements), N (memory)
 
-            use_prototypes = False
-            if use_prototypes:
-                prototype = features_c.mean(dim=0).unsqueeze(0)  # M=1, 256
-                features_c_norm = F.normalize(prototype, dim=1) # M=1, 256
-            else:
-                features_c_norm = F.normalize(features_c, dim=1) # M, 256
-            
+                
+                # now weight every sample
+                learned_weights_features = selector(features_c.detach()) # detach for trainability
+                learned_weights_features_memory = selector_memory(memory_c)
 
-            # compute similarity. All elements with all elements
-            similarities = torch.mm(features_c_norm, memory_c.transpose(1, 0))  # MxN
-            distances = 1 - similarities # values between [0, 2] where 0 means same vectors
-            # M (elements), N (memory)
+                # self-atention in the memory featuers-axis and on the learning contrsative featuers-axis
+                learned_weights_features = torch.sigmoid(learned_weights_features)
+                rescaled_weights = (learned_weights_features.shape[0] / learned_weights_features.sum(dim=0)) * learned_weights_features
+                rescaled_weights = rescaled_weights.repeat(1, distances.shape[1])
+                distances = distances * rescaled_weights
 
-            '''
-            # now weight every sample
-            learned_weights_features = selector(features_c.detach()) # detach for trainability
-            learned_weights_features_memory = selector_memory(memory_c)
+                learned_weights_features_memory = torch.sigmoid(learned_weights_features_memory)
+                learned_weights_features_memory = learned_weights_features_memory.permute(1, 0)
+                rescaled_weights_memory = (learned_weights_features_memory.shape[0] / learned_weights_features_memory.sum(dim=0)) * learned_weights_features_memory
+                rescaled_weights_memory = rescaled_weights_memory.repeat(distances.shape[0], 1)
+                distances = distances * rescaled_weights_memory
+                
 
-            # self-atention in the memory featuers-axis and on the learning contrsative featuers-axis
-            learned_weights_features = torch.sigmoid(learned_weights_features)
-            rescaled_weights = (learned_weights_features.shape[0] / learned_weights_features.sum(dim=0)) * learned_weights_features
-            rescaled_weights = rescaled_weights.repeat(1, distances.shape[1])
-            distances = distances * rescaled_weights
+                loss = loss + distances.mean()
 
-            learned_weights_features_memory = torch.sigmoid(learned_weights_features_memory)
-            learned_weights_features_memory = learned_weights_features_memory.permute(1, 0)
-            rescaled_weights_memory = (learned_weights_features_memory.shape[0] / learned_weights_features_memory.sum(dim=0)) * learned_weights_features_memory
-            rescaled_weights_memory = rescaled_weights_memory.repeat(distances.shape[0], 1)
-            distances = distances * rescaled_weights_memory
-            '''
+        else:
+            if memory_c is not None and features_c.shape[0] > 1 and memory_c.shape[0] > 1:
+                memory_c = torch.from_numpy(memory_c).cuda()
 
-            loss = loss + distances.mean()
+                # L2 normalize vectors
+                memory_c = F.normalize(memory_c, dim=1) # N, 256
+
+                use_prototypes = False
+                if use_prototypes:
+                    prototype = features_c.mean(dim=0).unsqueeze(0)  # M=1, 256
+                    features_c_norm = F.normalize(prototype, dim=1) # M=1, 256
+                else:
+                    features_c_norm = F.normalize(features_c, dim=1) # M, 256
+                
+                # compute similarity. All elements with all elements
+                similarities = torch.mm(features_c_norm, memory_c.transpose(1, 0))  # MxN
+                distances = 1 - similarities # values between [0, 2] where 0 means same vectors
+                # M (elements), N (memory)
+
+                loss = loss + distances.mean()
 
     return loss / num_classes
 
@@ -177,7 +197,7 @@ class AlonsoContrastiveLearner:
         proj_t_selected = proj_t[mask, :]
         
         if proj_t_selected.shape[0] > 0:
-            if self.mode == 'feat_quality':
+            if self.mode == 'full':
                 self.feature_memory.add_features(model, proj_t_selected, labels_t_down_selected, pred_t.shape[0], use_selector=True)
             elif self.mode == 'base':
                 self.feature_memory.add_features(model, proj_t_selected, labels_t_down_selected, pred_t.shape[0], use_selector=False)
@@ -236,7 +256,7 @@ class AlonsoContrastiveLearner:
                 loss_labeled = loss_labeled + contrastive_class_to_class(None, pred_s, labels_s_down, self.feature_memory.memory, use_selector=False)
             else:
                 raise Exception('Mode not available')
-                
+
         use_tl = False
         if use_tl:
 
