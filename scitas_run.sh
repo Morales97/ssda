71a065522591a704edbf23dd011497934c39443b
Morales97 <danimoralesbrotons@gmail.com>
2022-05-06 09:42:09 +0200

    .

--- scitas_run.sh
+++ scitas_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=seg_test
+#SBATCH --job-name=ce_mask
 #
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -8,35 +8,25 @@
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu_free
 #SBATCH --gres=gpu:1
-#SBATCH --mem=15000
-#SBATCH --time=02:00:00
+#SBATCH --mem=30000
+#SBATCH --time=04:00:00
 
-# ----- Fully supervised CS -----
-#python main.py --project=GTA_to_CS_tiny --expt_name=deeplab_rn50_FS_CS_pt_mask --net=deeplabv3_mask_pt --steps=5000
-#python main.py --project=GTA_to_CS_tiny --expt_name=only_100_CS --net=lraspp_mobilenet --steps=5000 --target_samples=100 
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=KL_ema_new --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl
+python main_SSDA.py --seed=2 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=CE_mask --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=ce --custom_pretrain=model/pretrained/ckpt_mask_dlrn50_CS_400.tar
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=alonso_full_tau0 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --alonso_contrast=full
+#python main_SSDA.py --seed=1 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=GJS_no_ema --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=gjs --n_augmentations=2
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=KL_no_ema_no_grad --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl --warmup_steps=5000
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=KL_oh_0.1 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl_oh --lmbda=0.1
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=JS_dsbn_l1 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=js --cr_ema=False --dsbn=True
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=JS_oh --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=js_oh --lmbda=0.01
 
-# ----- Domain Adaptation -----
-# -- DeepLab --
-# python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_PASCAL_lr3 --net=deeplabv3_mask_pt --target_samples=100 --custom_pretrain_path=model/pretrained/VOCSegmentation_supervised_saliency_model.pth.tar 
-# -- LR-ASPP --
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_100sup_top5_GTAcrops --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/ckpt_mask_CS_100sup_top5.tar
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_300_CS_GTA_GTAcrops --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/ckpt_mask_lraspp_CS_GTA_300.tar
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_600_scitas --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=mask_pt_CS_100sup_allclasses --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=model/pretrained/ckpt_mask_CS_100sup_allclasses.tar
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=no_pt_SCITAS_GTA_crops --net=lraspp_mobilenet --target_samples=100  
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=dsbn_l1_alonso_tu_0.9 --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --alonso_contrast=base --dsbn=True
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=alonso_FQ --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --alonso_contrast=feat_quality
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=CE_no_ema --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=ce --cr_ema=False #--custom_pretrainmodel/pretrained/ckpt_mask_dlrn50_CS_400.tar
+#python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=LAB_color --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --lab_color=True
+#python main_SSDA.py --seed=1 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --size=small --expt_name=KLE_GTA_cycada --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl
 
-# Rotation pretrained backbone
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=dummy2_100_same_size_rot_pt --net=lraspp_mobilenet --target_samples=100 --custom_pretrain_path=ckpt_rot_10k.tar
-
-# Rotation
-# python pretrain/rotation.py --batch_size=4 --lr=0.001 --project=rotation --expt_name=longer_run_lr_x10 --save_dir=pretrain/expts_rot/tmp_last --save_interval=2000 --steps=10001
-
-# ----- SSDA -----
-#python main_SSDA.py --project=GTA_to_CS_tiny --expt_name=CR_prob_distr_mask_pt_CS_NO_GTA --net=lraspp_mobilenet --target_samples=100 --cr=prob_distr --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
-#python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CL_mask_CS --seed=2 --net=lraspp_mobilenet_contrast --pixel_contrast=True --gamma=0.1 --pre_trained=True --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
-#python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CL_no_warmup_0.1 --net=lraspp_mobilenet_contrast --pixel_contrast=True --warmup_steps=0 --gamma=0.1
-
-python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_IN_bb_only_blurrer --net=lraspp_mobilenet --cr=prob_distr --seed=1
-#python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=CR_IN_bb_blurrer --seed=3 --net=lraspp_mobilenet --cr=prob_distr
-#python main_SSDA.py --project=GTA_CS_tiny_seeds --expt_name=PC_mask_CS --seed=3 --net=lraspp_mobilenet_contrast --custom_pretrain_path=model/pretrained/checkpoint_mask_lraspp_CS_600.pth.tar
+#python main_SSDA.py --seed=1 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --size=small --expt_name=CE_no_ema --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=ce
+#python main_SemiSup.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=100_CS --net=deeplabv3_rn50 --batch_size_tl=4 --batch_size_tu=4 
+#python main_SemiSup.py --seed=1 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --expt_name=SS_KLE --net=deeplabv2_rn101 --size=small --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl --warmup_steps=2000 
 
