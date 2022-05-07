dba6f0f895d7bf1c5abc6a9c53c1bb621fb2e7e4
Morales97 <danimoralesbrotons@gmail.com>
2022-05-07 18:10:31 +0200

    Update consistency.py

--- loss/consistency.py
+++ loss/consistency.py
@@ -229,10 +229,10 @@ def cr_KL_one_hot(p_w, p_s, tau=0.9, eps=1e-8):
     return loss_cr, percent_pl
 
 def cr_L2(p_w, p_s):
-    dist = torch.sqrt(pow(p_w - p_s, 2))
+    dist = torch.sqrt(torch.pow(p_w - p_s, 2))
     dist = dist.sum(axis=1)
     print(dist.mean())
-    pdb.set_trace()
+    #pdb.set_trace()
     return dist.mean(), 100
 
 def custom_kl_div(prediction, target):
