45ba2536f6ea87c845ba5162d91784a298f99522
Morales97 <danimoralesbrotons@gmail.com>
2022-05-07 21:09:45 +0200

    Update entropy_min.py

--- loss/entropy_min.py
+++ loss/entropy_min.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 def entropy_loss(out):
     n, c, h, w = out.size()
 
-    prob = F.softmax(out_cat, dim=1)
+    prob = F.softmax(out, dim=1)
     log_prob = torch.log(prob + 1e-10)
     entropy = - prob * log_prob
     entropy = torch.sum(entropy.view(-1)) / (n*h*w)
