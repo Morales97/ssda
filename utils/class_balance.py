e2126ee13ffacf7b3a40d2e3838b37a746eab44d
Morales97 <danimoralesbrotons@gmail.com>
2022-05-09 18:37:53 +0200

    Update class_balance.py

--- utils/class_balance.py
+++ utils/class_balance.py
@@ -25,8 +25,8 @@ def get_class_weights(dataloader, n_classes=19, precomputed=None):
     class_freq /= class_freq.sum()
     class_weight = np.sqrt(np.median(class_freq) / class_freq)
     print('Class weighting time [s]: ' + str(time.time()-ts))
-    print(class_weight.round(2))
-    return torch.Tensor(class_weight)
+    print(class_weight.round(4))
+    return torch.Tensor(class_weight).to('cuda')
 
 # weights rounded to 2 for 100 CS samples (seed 1)
 # array([0.16, 0.37, 0.21, 0.87, 1.03, 0.89, 2.24, 1.28, 0.24, 0.93, 0.45, 1.  , 2.79, 0.39, 1.8 , 2.35, 1.97, 5.57, 1.99])
