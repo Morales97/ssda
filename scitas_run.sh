6975c536893e36eaf8df71ae6bc3acfc5acde9d5
Morales97 <danimoralesbrotons@gmail.com>
2022-05-09 16:10:40 +0200

    Update scitas_run.sh

--- scitas_run.sh
+++ scitas_run.sh
@@ -26,7 +26,7 @@
 #python main_SSDA.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=LAB_color --net=deeplabv3_rn50 --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --lab_color=True
 #python main_SSDA.py --seed=1 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --size=small --expt_name=KLE_GTA_cycada --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl
 
-python main_SSDA.py --save_model=False --seed=3 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=5000 --project=GTA_to_CS_small --size=small --expt_name=KL_full_equip --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl --alonso_contrast=full --pixel_contrast=True --pc_mixed=True --cutmix_cr=True --cutmix_sup=True --custom_pretrain_path=model/pretrained/ckpt_mask_v2.tar
+python main_SSDA.py --save_model=False --seed=3 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=5000 --project=GTA_to_CS_small --size=small --expt_name=KL_full_equip --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl --alonso_contrast=full --pixel_contrast=True --pc_mixed=True --cutmix_cr=True --cutmix_sup=True --custom_pretrain=model/pretrained/ckpt_mask_v2.tar
 #python main_SemiSup.py --seed=1 --lr=0.001 --lr_decay=det --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --expt_name=100cs --net=deeplabv2_rn101 --size=small --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl --alonso_contrast=full --pixel_contrast=True --pc_mixed=True 
 #python main_SemiSup.py --seed=3 --save_model=False --project=GTA_CS_rn50_tiny --expt_name=100_CS --net=deeplabv3_rn50 --batch_size_tl=4 --batch_size_tu=4 
 
