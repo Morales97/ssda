ab4ffe1262f673510e77dd658ad5e6fff3a1b3fe
Morales97 <danimoralesbrotons@gmail.com>
2022-05-09 18:17:35 +0200

    Update class_balance.py

--- utils/class_balance.py
+++ utils/class_balance.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pdb 
+import time
 
 def get_class_weights(dataloader, n_classes=19):
 
+    ts = time.time()
     class_freq = np.zeros(n_classes)
 
     for _, labels in dataloader:
@@ -11,8 +13,11 @@ def get_class_weights(dataloader, n_classes=19):
     
     class_freq /= class_freq.sum()
     class_weight = np.sqrt(np.median(class_freq) / class_freq)
-
+    print('Class weighting time [s]: ' + str(time.time()-ts))
     pdb.set_trace()
 
 # weights rounded to 2 for 100 CS samples (seed 1)
 # array([0.16, 0.37, 0.21, 0.87, 1.03, 0.89, 2.24, 1.28, 0.24, 0.93, 0.45, 1.  , 2.79, 0.39, 1.8 , 2.35, 1.97, 5.57, 1.99])
+
+# weights rounded to 2 for 100 CS samples (seed 2)
+#array([0.16, 0.38, 0.21, 1.22, 1.  , 0.9 , 2.2 , 1.41, 0.24, 0.85, 0.44, 0.92, 2.6 , 0.38, 1.89, 2.  , 1.56, 2.23, 1.51])
