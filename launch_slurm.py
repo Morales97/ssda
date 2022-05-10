98b98d5a29cf1f1965ea0e2c4a936879162cbc75
Morales97 <danimoralesbrotons@gmail.com>
2022-05-10 17:35:04 +0200

    Update launch_slurm.py

--- launch_slurm.py
+++ launch_slurm.py
@@ -1,6 +1,6 @@
-import commands
+import subprocess
 
 if __name__ == '__main__':
     print('running launch_slurm.py...')
     cmd = 'sbatch toy_job.sh'
-    status, output = commands.getstatusoutput(cmd)
+    status, output = subprocess.getstatusoutput(cmd)
