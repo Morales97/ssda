8c53bf4c55bccd808238be91854f57b3319a577e
Morales97 <danimoralesbrotons@gmail.com>
2022-05-11 15:54:23 +0200

    Update cityscapes_ds.py

--- loader/cityscapes_ds.py
+++ loader/cityscapes_ds.py
@@ -275,7 +275,7 @@ class cityscapesDataset(data.Dataset):
                 # get image
                 img_path = img_path.rstrip()
                 img = pil_loader(img_path, self.img_size[0], self.img_size[1])
-                img = self.transforms(img)[0].unsqueeze(0)
+                img = self.transforms(img)[0].unsqueeze(0).cuda()
 
                 # generate pseudolabel
                 pred = model(img)['out']
