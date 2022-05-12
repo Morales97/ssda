21884380933e4867ffeb8c20757cf6a4455ea667
Morales97 <danimoralesbrotons@gmail.com>
2022-05-12 14:31:44 +0200

    Update test_loader.py

--- test_loader.py
+++ test_loader.py
@@ -27,7 +27,7 @@ import wandb
 import tqdm
 from model.model import get_model
 from torch_ema import ExponentialMovingAverage
-from loader.loaders import get_loaders, generate_pseudolabels
+from loader.loaders import get_loaders
 import pdb
 
 def main(args, wandb):
