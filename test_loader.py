c8b244c92e015f3d89c9c5c93bff295c200ae0e8
Morales97 <danimoralesbrotons@gmail.com>
2022-05-12 09:50:46 +0200

    Update test_loader.py

--- test_loader.py
+++ test_loader.py
@@ -90,6 +90,10 @@ if __name__ == '__main__':
 
     source_loader, target_loader, target_loader_unl, val_loader, target_dataset = get_loaders(args)
 
+    for img, lbl in target_loader:
+        pdb.set_trace()
+        break
+
     os.makedirs('data/cityscapes/pseudo_labels/test', exist_ok=True)
     target_dataset.pseudolabel_folder = 'test_pl'
     target_dataset.generate_pseudolabels(model, ema)
