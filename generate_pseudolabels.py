000060cb9b012d921630a7a63c7c2ca85611c5a6
Morales97 <danimoralesbrotons@gmail.com>
2022-05-16 11:31:58 +0200

    Update generate_pseudolabels.py

--- generate_pseudolabels.py
+++ generate_pseudolabels.py
@@ -9,7 +9,7 @@ import torch
 
 from utils.ioutils import parse_args
 from model.model import get_model
-from loader.loaders import get_loaders, generate_pseudolabels
+from loader.loaders import get_loaders
 from torch_ema import ExponentialMovingAverage # https://github.com/fadel/pytorch_ema 
 import pdb
 
