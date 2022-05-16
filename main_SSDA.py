c0486e5521651b855444fb69a25bbaca88799a49
Morales97 <danimoralesbrotons@gmail.com>
2022-05-16 18:32:23 +0200

    Update main_SSDA.py

--- main_SSDA.py
+++ main_SSDA.py
@@ -100,10 +100,7 @@ def main(args, wandb):
 
     # Set up metrics
     _initialize()
-    data_iter_s = iter(source_loader)
-    data_iter_t = iter(target_loader)
-    data_iter_t_unl = iter(target_loader_unl)
-    
+
     # Training loop
     while step <= args.steps:
 
