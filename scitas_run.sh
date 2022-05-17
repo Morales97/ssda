def292236003a057353a931ac0487c6727c543d5
Morales97 <danimoralesbrotons@gmail.com>
2022-05-17 21:04:22 +0200

    Update scitas_run.sh

--- scitas_run.sh
+++ scitas_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=mem
+#SBATCH --job-name=r4
 #
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -12,7 +12,6 @@
 #SBATCH --time=12:00:00
 
 
-python test_ensemble.py
 #python main_SSDA.py --seed=2 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_mem --cr=ce --pixel_contrast=True --pc_memory=True
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_CMcr_gaus_blur --cr=ce --pixel_contrast=True --aug_level=5 --cutmix_cr=True
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_CMsup --cr=ce --pixel_contrast=True --cutmix_sup=True
@@ -33,6 +32,7 @@ python test_ensemble.py
 #python main_SemiSupNEW.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=KL_pc_cw_PL_noS --cr=kl --pixel_contrast=True --pseudolabel_folder=KL_pc_40k3_test
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=KL_pc_cw_PL_2 --cr=kl --pixel_contrast=True --pseudolabel_folder=KL_pc_40k3_test
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=KL_pc_cw_r3 --cr=kl --pixel_contrast=True --pseudolabel_folder=KL_pc_r23_test
+python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=KL_pc_cw_r4 --cr=kl --pixel_contrast=True --pseudolabel_folder=KL_r3_noPL3
 
 # UDA
 #python main_UDA.py --seed=3 --steps=40000 --save_interval=10000 --project=GTA_to_CS_small --expt_name=UDA_CE_alo --cr=ce --alonso_contrast=full
