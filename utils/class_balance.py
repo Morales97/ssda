e10bfe502281c210f793ff2c39c8a2a072001c15
Morales97 <danimoralesbrotons@gmail.com>
2022-05-17 11:47:34 +0200

    Update class_balance.py

--- utils/class_balance.py
+++ utils/class_balance.py
@@ -77,8 +77,8 @@ def get_class_weights_estimation(dataloader_lbl, dataloader_unlbl, model, ema, n
 if __name__ == '__main__':
     import os
     print(os.getcwd())
-    path_parent = os.path.dirname(os.getcwd())
-    os.chdir(path_parent)
+    #path_parent = os.path.dirname(os.getcwd())
+    #os.chdir(path_parent)
     print(os.getcwd())
     from loader.cityscapes_ds import cityscapesDataset
     from torch.utils.data import DataLoader
