1e0128d99870f7b584bb26a2e86e365ff9fe7c15
Morales97 <danimoralesbrotons@gmail.com>
2022-05-17 20:36:58 +0200

    .

--- scitas_run.sh
+++ scitas_run.sh
@@ -12,7 +12,8 @@
 #SBATCH --time=12:00:00
 
 
-python main_SSDA.py --seed=2 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_mem --cr=ce --pixel_contrast=True --pc_memory=True
+python test_ensemble.py
+#python main_SSDA.py --seed=2 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_mem --cr=ce --pixel_contrast=True --pc_memory=True
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_CMcr_gaus_blur --cr=ce --pixel_contrast=True --aug_level=5 --cutmix_cr=True
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=CE_pc_cw_CMsup --cr=ce --pixel_contrast=True --cutmix_sup=True
 #python main_SSDA.py --seed=3 --steps=40000 --save_interval=40000 --steps_job=20000 --project=GTA_to_CS_small --expt_name=KL_pc_cw_PL --cr=kl --pixel_contrast=True --pseudolabel_folder=KL_pc_40k3_test
--- test_ensemble.py
+++ test_ensemble.py
@@ -7,6 +7,7 @@ from torch_ema import ExponentialMovingAverage
 from evaluation.metrics import averageMeter, runningScore
 from loss.cross_entropy import cross_entropy2d
 from loader.loaders import get_loaders
+from collections import OrderedDict
 
 def _log_validation_ema(model, ema, val_loader, loss_fn, step, wandb=None):
     running_metrics_val = runningScore(19)
