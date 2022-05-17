eff1405559ceed0c376b5cafc451c2dbd0f727d3
Morales97 <danimoralesbrotons@gmail.com>
2022-05-17 17:19:42 +0200

    Update main_SSDA.py

--- main_SSDA.py
+++ main_SSDA.py
@@ -77,7 +77,16 @@ def main(args, wandb):
             start_step = checkpoint['step']
             print('*** Loading checkpoint from ', args.resume)
             print('*** Resuming from train step {}'.format(start_step))
+            pdb.set_trace()
+            data_iter_t = iter(target_loader)
+            images_t, labels_t = next(data_iter_t)
+            model.eval()
+            out1 = model(images_t)
+            with ema.average_parameters():
+                out2 = model(images_t)
             score = _log_validation(model, val_loader, loss_fn, start_step, wandb)
+            score = _log_validation_ema(model, ema, val_loader, loss_fn, start_step, wandb)
+
         else:
             raise Exception('No file found at {}'.format(args.resume))
     step = start_step
@@ -495,14 +504,14 @@ if __name__ == '__main__':
 
 
 
-# python main_SSDA.py --net=deeplabv3_rn50 --wandb=False --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --pixel_contrast=True --pc_memory=True --pc_mixed=True --warmup_steps=0
-# python main_SSDA.py --net=deeplabv3_rn50 --wandb=False --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl --cutmix_sup=True
-# python main_SSDA.py --net=deeplabv3_rn50 --wandb=False --log_interval=1 --val_interval=1 --save_interval=1
-# python main_SSDA.py --net=deeplabv3_rn50 --wandb=False --alonso_contrast=full --warmup_steps=0 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=js --cr_ema=False
-# python main_SSDA.py --net=deeplabv2_rn101 --wandb=False --alonso_contrast=full --pixel_contrast=True --warmup_steps=0 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 
-# python main_SSDA.py --net=deeplabv2_rn101 --wandb=False --batch_size_s=1 --batch_size_tl=1 --batch_size_tu=1 --cr=ce --pixel_contrast=True --pc_mixed=True --alonso_contrast=full --warmup_steps=0
-# python main_SSDA.py --net=deeplabv2_rn101 --size=small --expt_name=test_pl --wandb=False --resume=model/pretrained/checkpoint_KLE1_p2.pth.tar
+# python main_SSDA.py --size=tiny --net=deeplabv3_rn50 --wandb=False --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --pixel_contrast=True --pc_memory=True --pc_mixed=True --warmup_steps=0
+# python main_SSDA.py --size=tiny --net=deeplabv3_rn50 --wandb=False --batch_size_s=4 --batch_size_tl=4 --batch_size_tu=4 --cr=kl --cutmix_sup=True
+# python main_SSDA.py --size=tiny --net=deeplabv3_rn50 --wandb=False --log_interval=1 --val_interval=1 --save_interval=1
+# python main_SSDA.py --size=tiny --net=deeplabv3_rn50 --wandb=False --alonso_contrast=full --warmup_steps=0 --cr=js --cr_ema=False
+# python main_SSDA.py --wandb=False --alonso_contrast=full --pixel_contrast=True --warmup_steps=0 
+# python main_SSDA.py --wandb=False --batch_size_s=1 --batch_size_tl=1 --batch_size_tu=1 --cr=ce --pixel_contrast=True --pc_mixed=True --alonso_contrast=full --warmup_steps=0
+# python main_SSDA.py --wandb=False --resume=expts/tmp_last/checkpoint_KL_pc_cw_r3_3.pth.tar
 
 # next round of ST
-#python main_SSDA.py --seed=1 --wandb=False --size=small --expt_name=KL_pc_round2 --net=deeplabv2_rn101 --batch_size_s=2 --batch_size_tl=2 --batch_size_tu=2 --cr=kl --pixel_contrast=True --pc_mixed=True --warmup_steps=0 --pseudolabel_folder=test_pl1_test --round_start=model/pretrained/checkpoint_KLE1_p2.pth.tar
+#python main_SSDA.py --seed=1 --wandb=False --size=small --expt_name=KL_pc_round2 --cr=kl --pixel_contrast=True --pc_mixed=True --warmup_steps=0 --pseudolabel_folder=test_pl1_test --round_start=model/pretrained/checkpoint_KLE1_p2.pth.tar
 
