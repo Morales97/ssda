da430d686ea887d004d738335047f1b7b84fc36d
Morales97 <danimoralesbrotons@gmail.com>
2022-05-22 19:19:58 +0200

    .

--- generate_pl.sh
+++ generate_pl.sh
@@ -12,7 +12,7 @@
 #SBATCH --time=12:00:00
 
 # SSDA
-#python generate_pseudolabels.py --seed=1 --expt_name=full --resume=expts/tmp_last/checkpoint_full_1.pth.tar
+python generate_pseudolabels.py --seed=1 --expt_name=full_r2 --resume=expts/tmp_last/checkpoint_full_r2_p2_1.pth.tar
 
 # SS
 #python generate_pseudolabels.py --seed=3 --expt_name=SS_CE_pc_mem_r2 --resume=expts/tmp_last/checkpoint_SS_CE_pc_mem_r2_3.pth.tar --pc_memory=True
@@ -21,4 +21,4 @@
 #python generate_pseudolabels.py --target_samples=0 --expt_name=UDA_CE --resume=expts/tmp_last/checkpoint_UDA_CE_3.pth.tar
 
 # evaluate
-python evaluate.py 
+#python evaluate.py 
