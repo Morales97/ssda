a3de582d2499e79ec828a49f06b440530113a7fd
Morales97 <danimoralesbrotons@gmail.com>
2022-06-04 11:34:38 +0200

    Update tsne.py

--- tsne.py
+++ tsne.py
@@ -59,13 +59,29 @@ def main(args):
 
     model.eval()
 
-    for image, label in val_loader:
-        image = image.cuda()
-        label = label.cuda()
-        
-        out = model(image)
-        feat = out['feat']
-        pdb.set_trace()
+    with torch.no_grad():
+        X = np.zeros((19, 100, 2048))
+        Y = np.zeros((19, 100))
+        class_ptr = np.zeros((19))
+        for image, label in val_loader:
+            image = image.cuda()
+            label = label.cuda()
+            input_shape = label.shape[1:]
+
+            out = model(image)
+            feat = out['feat']
+            feat = F.interpolate(feat, size=input_shape, mode="bilinear", align_corners=False)
+
+            # select one pixel from every class present in the image. repeat until all pixel classes are filled
+            for c in range(19):
+                if class_ptr[c] >= 100:
+                    pass
+                else:
+                    idxs = np.where(label == c)
+                    X[c, class_ptr] = feat[0, :, idxs[1], idxs[:2]]
+                    Y[c, class_ptr] = c     # we dont need Y at all
+                        
+            pdb.set_trace()
 
 
 if __name__ == '__main__':
