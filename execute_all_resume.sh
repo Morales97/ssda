a5c78221e862a86b1830c84db1d7dcbc1a5ab1d6
Morales97 <danimoralesbrotons@gmail.com>
2022-06-08 20:56:44 +0200

    ,

--- execute_all_resume.sh
+++ execute_all_resume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sbatch --dependency=afterok:$1 seed_resume.sh 1
-sbatch --dependency=afterok:$1+1 seed_resume.sh 2
-sbatch seed_resume.sh 3
+sbatch --dependency=afterok:$(($1+1)) seed_resume.sh 2
+sbatch --dependency=afterok:$(($1+2)) seed_resume.sh 3
