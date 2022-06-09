1dd46deceb98d469ca6568046e3151aeca0fe3b5
Morales97 <danimoralesbrotons@gmail.com>
2022-06-09 18:54:10 +0200

    .

--- seed_launch.sh
+++ seed_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=200r2
+#SBATCH --job-name=500r2
 #
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -11,6 +11,6 @@
 #SBATCH --mem=30000
 #SBATCH --time=12:00:00
 
-python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_200_r2 --target_samples=200 --cr=ce --pixel_contrast=True --pseudolabel_folder=full_200_s$1
+python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_500_r2 --target_samples=500 --cr=ce --pixel_contrast=True --pseudolabel_folder=full_500_s$1
 #python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_2975 --target_samples=2975 --cr=ce --pixel_contrast=True
 #python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_744 --target_samples=744 --cr=ce --pixel_contrast=True
