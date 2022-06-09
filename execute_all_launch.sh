7e7b8efcdd16ad9d0e7a93c33ab8e9ed2fbabe9b
Morales97 <danimoralesbrotons@gmail.com>
2022-06-09 17:00:00 +0200

    .

--- execute_all_launch.sh
+++ execute_all_launch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sbatch seed_launch.sh 1
-sbatch seed_launch.sh 2
-sbatch seed_launch.sh 3
+sbatch seed_resume.sh 1
+sbatch seed_resume.sh 2
+sbatch seed_resume.sh 3
+
+#sbatch seed_launch.sh 1
+#sbatch seed_launch.sh 2
+#sbatch seed_launch.sh 3
--- seed_resume.sh
+++ seed_resume.sh
@@ -11,5 +11,5 @@
 #SBATCH --mem=30000
 #SBATCH --time=12:00:00
 
-#python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_500 --target_samples=500 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_full_500_$1.pth.tar
-python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_744 --target_samples=744 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_SemiSup_744_$1.pth.tar
+python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_200 --target_samples=200 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_full_500_$1.pth.tar
+#python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_744 --target_samples=744 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_SemiSup_744_$1.pth.tar
