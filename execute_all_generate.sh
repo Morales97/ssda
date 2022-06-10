0c5416a87e6a81272df9493863add81a436e221f
Morales97 <danimoralesbrotons@gmail.com>
2022-06-10 10:06:53 +0200

    Update execute_all_generate.sh

--- execute_all_generate.sh
+++ execute_all_generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sbatch --dependency=afterok:$1 generate_pl.sh
-sbatch --dependency=afterok:$(($1+1)) generate_pl.sh
-sbatch --dependency=afterok:$(($1+2)) generate_pl.sh
+sbatch --dependency=afterok:$1 generate_pl.sh 1
+sbatch --dependency=afterok:$(($1+1)) generate_pl.sh 2
+sbatch --dependency=afterok:$(($1+2)) generate_pl.sh 3
 
