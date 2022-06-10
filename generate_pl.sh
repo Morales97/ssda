31770d109e523965ef394396a63a2d8c338e3119
Morales97 <danimoralesbrotons@gmail.com>
2022-06-10 13:35:57 +0200

    Update generate_pl.sh

--- generate_pl.sh
+++ generate_pl.sh
@@ -12,7 +12,8 @@
 #SBATCH --time=12:00:00
 
 # SSDA
-python generate_pseudolabels.py --seed=$1 --expt_name=full_200 --target_samples=200 --resume=expts/tmp_last/checkpoint_full_200_$1.pth.tar
+#python generate_pseudolabels.py --seed=$1 --expt_name=full_200 --target_samples=200 --resume=expts/tmp_last/checkpoint_full_200_$1.pth.tar
+python generate_pseudolabels.py --seed=$1 --expt_name=SS_200 --target_samples=200 --resume=expts/tmp_last/checkpoint_full_200_$1.pth.tar
 
 # SS
 #python generate_pseudolabels.py --seed=3 --expt_name=SSnomem_r2 --resume=expts/tmp_last/checkpoint_SemiSup_nomem_p2_3.pth.tar
