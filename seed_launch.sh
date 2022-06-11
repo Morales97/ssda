3d11be49d9b32dd67781049408f6ea8c0b8f27a6
Morales97 <danimoralesbrotons@gmail.com>
2022-06-11 18:55:53 +0200

    Update seed_launch.sh

--- seed_launch.sh
+++ seed_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=d200
+#SBATCH --job-name=d500
 #
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -17,5 +17,5 @@
 
 #python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_2975 --target_samples=2975 --cr=ce --pixel_contrast=True
 #python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_r2_noPL --cr=ce --pixel_contrast=True --pseudolabel_folder=SSnomem_s$1
-python main_SSDA.py --seed=$1 --project=clean_runs --expt_name=abl_d200 --pixel_contrast=False --target_samples=200
+python main_SSDA.py --seed=$1 --project=clean_runs --expt_name=abl_d500 --pixel_contrast=False --target_samples=500
 
