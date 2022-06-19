9dd9f72161e1b54d498fed7724dcf5e72d31b3ad
Morales97 <danimoralesbrotons@gmail.com>
2022-06-19 22:08:29 +0200

    .

--- seed_resume.sh
+++ seed_resume.sh
@@ -18,4 +18,4 @@
 
 #python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_2975 --target_samples=2975 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_SemiSup_2975_$1.pth.tar
 #python main_SemiSup.py --seed=$1 --steps_job=20000 --project=clean_SSL --expt_name=SemiSup_744_r2_nodrop --target_samples=744 --cr=ce --pixel_contrast=True --resume=expts/tmp_last/checkpoint_SemiSup_744_r2_nodrop_$1.pth.tar --pseudolabel_folder=SS_744_s$1
-python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_500_r2_nodrop --target_samples=500 --cr=ce --pixel_contrast=True --pseudolabel_folder=full_500_s$1 --resume=expts/tmp_last/checkpoint_full_500_r2_nodrop$1.pth.tar 
+python main_SSDA.py --seed=$1 --steps_job=20000 --project=clean_runs --expt_name=full_500_r2_nodrop --target_samples=500 --cr=ce --pixel_contrast=True --pseudolabel_folder=full_500_s$1 --resume=expts/tmp_last/checkpoint_full_500_r2_nodrop_$1.pth.tar 
